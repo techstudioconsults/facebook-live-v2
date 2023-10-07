07ffad83c14347f10fc453fb0176f3ccb6a96968
Intuneteq <tobiolanitori@gmail.com>
2023-10-07 06:35:46 +0100

    ci: updated server.cjs -2

--- server.cjs
+++ server.cjs
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(9000)
